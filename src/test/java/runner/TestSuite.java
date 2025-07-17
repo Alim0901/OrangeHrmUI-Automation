10d0b5b1511b3e058a9633b4a4a1ce996260979a
alim <alim.batur36@gmail.com>
2025-07-17 23:27:32 +1000

    changed the framework to achieve parallel execution

--- src/test/java/runner/TestSuite.java
+++ src/test/java/runner/TestSuite.java
@@ -2,10 +2,12 @@ package runner;
 
 import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
+@IncludeEngines("cucumber")                 // Required for Cucumber to be recognized by JUnit platform
 @SelectClasspathResource("features")       // looks under src/test/resources/features
 @ConfigurationParameter(
         key   = Constants.GLUE_PROPERTY_NAME,
@@ -18,8 +20,14 @@ import org.junit.platform.suite.api.Suite;
                 "junit:target/cucumber-results.xml," +
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 )
+
 @ConfigurationParameter(
         key   = Constants.FILTER_TAGS_PROPERTY_NAME,
         value = "@smoke"
 )
+@ConfigurationParameter(
+        key   = "cucumber.execution.parallel.enabled",
+        value = "true"
+)
+
 public class TestSuite { }
--- src/test/java/utils/XMLConfigLoader.java
+++ src/test/java/utils/XMLConfigLoader.java
@@ -4,36 +4,47 @@ import org.w3c.dom.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class XMLConfigLoader {
 
-    private static Document document;
+    private static final Map<String, String> configMap = new HashMap<>();
 
     static {
         try {
             File configFile = new File("src/test/resources/config/config.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = dbFactory.newDocumentBuilder();
-            document = builder.parse(configFile);
-            document.getDocumentElement().normalize();
+            Document doc = builder.parse(configFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList childNodes = doc.getDocumentElement().getChildNodes();
+            for (int i = 0; i < childNodes.getLength(); i++) {
+                Node node = childNodes.item(i);
+                if (node instanceof Element element) {
+                    String key = element.getTagName();
+                    String value = element.getTextContent().trim();
+                    configMap.put(key, value);
+                }
+            }
         } catch (Exception e) {
             throw new RuntimeException("Failed to load config.xml", e);
         }
     }
 
-    public static String get(String tagName) {
-        NodeList nodeList = document.getElementsByTagName(tagName);
-        if (nodeList.getLength() == 0) {
-            throw new RuntimeException("Tag not found in config.xml: " + tagName);
+    public static String get(String key) {
+        if (!configMap.containsKey(key)) {
+            throw new RuntimeException("Tag not found in config.xml: " + key);
         }
-        return nodeList.item(0).getTextContent();
+        return configMap.get(key);
     }
 
-    public static boolean getBoolean(String tagName) {
-        return Boolean.parseBoolean(get(tagName));
+    public static boolean getBoolean(String key) {
+        return Boolean.parseBoolean(get(key));
     }
 
-    public static int getInt(String tagName) {
-        return Integer.parseInt(get(tagName));
+    public static int getInt(String key) {
+        return Integer.parseInt(get(key));
     }
 }
